ce9750eb40cb4f8709d7cd9b96ac2cd978edad1c
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-02-28 12:25:06 +0600

    Tab bar and navigation

---
+++ GHFollowers/FavouritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavouritesListVC.swift
+//  GHFollowers
+//
+//  Created by Mahmudul Hasan on 27/2/24.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Mahmudul Hasan on 27/2/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+        
+    }
+    
+
+}
