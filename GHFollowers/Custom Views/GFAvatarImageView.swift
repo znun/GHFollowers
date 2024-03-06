9adfdeb87d53b434dc7115af6fc73c08f58d0e8c
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-03-06 21:06:38 +0600

    Design imageview and label for follower

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,34 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Mahmudul Hasan on 6/3/24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
--- GHFollowers/Model/Follower.swift
+++ GHFollowers/Model/Follower.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Followers: Codable {
+struct Follower: Codable {
     var login: String
     var avatarUrl: String
 }
