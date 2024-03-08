410231132b744e54d8d97e21000b845b020b54a1
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-03-08 12:03:59 +0600

    Store images on Cache

--- GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -10,8 +10,8 @@ import UIKit
 class GFAvatarImageView: UIImageView {
     
     let placeholderImage = UIImage(named: "avatar-placeholder")!
-
-//let placeholderImage = UIImage(systemName: "person.circle")
+    
+    let cache = NetworkManager.shared.cache
                                    
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,6 +33,14 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from urlString: String) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+            
         guard let url = URL(string: urlString) else {return}
         
         let task = URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
@@ -45,8 +53,9 @@ class GFAvatarImageView: UIImageView {
             guard let data = data else {return}
             guard let image = UIImage(data: data) else {return}
             
-            DispatchQueue.main.async {
+            self.cache.setObject(image, forKey: cacheKey)
             
+            DispatchQueue.main.async {
                 self.image = image
             }
         }
--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Mahmudul Hasan on 5/3/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
     
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
